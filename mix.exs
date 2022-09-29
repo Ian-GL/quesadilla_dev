2c6b636902e80143f5a29912e127afd34c5f2db0
Ian Gonzalez <ian.gl@protonmail.com>
2022-09-28 22:34:21 -0500

    Update deps

--- mix.exs
+++ mix.exs
@@ -33,23 +33,23 @@ defmodule QuesadillaDev.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.6"},
+      {:phoenix, "~> 1.6.12"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
+      {:ecto_sql, "~> 3.9"},
+      {:postgrex, "~> 0.16.5"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.17.5"},
+      {:phoenix_live_view, "~> 0.18.0"},
       {:finch, "~> 0.13"},
       {:floki, ">= 0.30.0"},
-      {:phoenix_live_dashboard, "~> 0.6"},
+      {:phoenix_live_dashboard, "~> 0.7"},
       {:esbuild, "~> 0.3", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
+      {:swoosh, "~> 1.8"},
+      {:tailwind, "~> 0.1.9", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.18"},
-      {:jason, "~> 1.2"},
+      {:gettext, "~> 0.20"},
+      {:jason, "~> 1.4"},
       {:plug_cowboy, "~> 2.5"}
     ]
   end
